33018cdddc830ccd066ca75eca0a7997e91bc9a4
P16223631 <p16223631@my365.dmu.ac.uk>
2019-08-12 19:18:42 +0100

    ...

--- BI-coursework/BI-coursework/Form1.cs
+++ BI-coursework/BI-coursework/Form1.cs
@@ -382,7 +382,7 @@ namespace BI_coursework
             // Create A List To Store Products In
             List<string> Products = new List<string>();
             // Clear Items
-            Products.Items.Clear();
+            //Products.Items.Clear();
 
             // Create Database String 
             string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
